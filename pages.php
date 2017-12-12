af243306eaf0c11c94b1b7c5250fe00b9ed0e828
kolorowestudio <kolorowestudio@gmail.com>
2017-12-12 18:55:04 +0100

    pages file example

--- pages.php
+++ pages.php
@@ -9,16 +9,12 @@
       'page_method' => 'GET', // GET, POST
       'page_url' => '/', // page
       'page_source' => 'pages/home-page.php', // page URL - must be unique!
-      'page_title' => 'title', // SEO Title
-      'page_desc' => 'desc' // SEO Description
     ],
     [
       'page_id' => 'test',
       'page_method' => 'GET',
       'page_url' => '/test-page',
-      'page_source' => 'pages/test-page.php',
-      'page_title' => 'title test',
-      'page_desc' => 'desc test'
+      'page_source' => 'pages/test-page.php'
     ]
   ];
 
